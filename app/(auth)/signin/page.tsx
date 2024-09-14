6a9f717a6200019883682337cb00c75476b9eb27
Akshaya Mohan <akshayamohan.2401@gmail.com>
2024-09-14 09:29:09 -0700

    Mobile

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -44,29 +44,33 @@ const Signin = () => {
 
   return (
     <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-         <div className="text-white mr-10 mt-20 font-mono font-bold text-9xl">
-          
-          <span className="text-6xl "> Nano </span><br></br> Discord
-         </div>
-         <div className="text-blue-400 mt-5 text-xl font-light font-sans italic">
-       powered by Gemini AI
-        </div>
-        <div>
-            <Button onClick={handleClick} size="lg"  className="text-xl mt-10 bg-white font-mono  text-black"> LOGIN </Button>
-        </div>
-        {/* Nano Discord <br /> Powered by AI */}
-      </motion.h1>
-    </LampContainer>
+    <motion.h1
+      initial={{ opacity: 0.5, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-7xl"
+    >
+      <div className="text-white mr-10 mt-20 font-mono font-bold text-7xl ml-10 md:ml-0 md:text-9xl">
+        <span className="md:text-6xl text-5xl">Nano</span><br></br>Discord
+      </div>
+      <div className="text-blue-500 mt-5 text-xl font-light font-sans italic sm:text-lg">
+        powered by Gemini AI
+      </div>
+      <div>
+        <Button
+          onClick={handleClick}
+          size="lg"
+          className="text-xl mt-10 bg-white font-mono text-black sm:text-lg"
+        >
+          LOGIN
+        </Button>
+      </div>
+    </motion.h1>
+  </LampContainer>
   );
 };
 
